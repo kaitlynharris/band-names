22c7605f7442a75aea81cc6f8270ef877acdfb70
kaitlynharris <kaitlynhikes@gmail.com>
2016-05-03 16:34:00 -0700

    removed for

--- bandnamefunctions.py
+++ bandnamefunctions.py
@@ -2,7 +2,7 @@ import random
 
 adjectives = ['Neutral', 'Future', 'Mad', 'Bright', 'Another', 'Red', 'Revolving', 'Polar', 'Pretty', 'Grand', '13th', 'Old', 'Broken', 'Public', 'Artful', 'British', 'Legendary', 'Acid', 'Astral']
 
-flex = ['Magic', 'Control', 'Analog', 'For', 'Chaos', 'Iron', 'Girl', 'Boy', 'Assault', 'Social', 'Colour', 'Mortal', 'Jazz', 'Chemical', 'Midnight', 'Mountain', 'River', 'Hellfire']
+flex = ['Magic', 'Control', 'Analog', 'Forest', 'Chaos', 'Iron', 'Girl', 'Boy', 'Assault', 'Social', 'Colour', 'Mortal', 'Jazz', 'Chemical', 'Midnight', 'Mountain', 'River', 'Hellfire']
 
 nouns = ['Hotel', 'Sister', 'Fire', 'Mission', 'Club', 'Shelter', 'Day', 'Rope', 'Orchestra', 'Dream', 'Union', 'Winter', 'Cult', 'Gang', 'Group', 'Incorporated', 'Crew', 'Clan', 'Experience']
 
